a0fea10be978bfac07d8a570c4586423caed200b
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-18 19:29:38 -0600

    commit tree 60be678b10e8f0c9d4952825d7333ca8255d4089

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  #index
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  #new
+  get '/articles/new' do
+    erb :new 
+  end
+
+  #create
+  post '/articles' do
+    @article = Article.create(params)
+    
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #Show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #edit
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  #update
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+
+  end
+
+  #delete
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
+
 end
